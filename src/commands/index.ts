ccdd37497770b218c884841a6872cb2d68c9ed9a
Michele31 <info@mineperfumelab.com>
2026-06-23 15:54:46 +0200

    fix: broaden Command.data type to SlashCommandOptionsOnlyBuilder

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,16 @@
-import { Collection, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  Collection,
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+} from 'discord.js';
 import * as wipe from './wipe';
 import * as attendance from './attendance';
 import * as exportCmd from './export';
 import * as closewipe from './closewipe';
 
 export interface Command {
-  data: SlashCommandBuilder;
+  data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
   execute(interaction: ChatInputCommandInteraction): Promise<void>;
 }
 
